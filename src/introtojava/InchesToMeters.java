0cd65f6865fe297eeb796ec7f0c5cb3e314a20ae
fatumaIsmail <fatumaismail38@gmail.com>
2023-01-29 11:49:43 -0500

    added class InchesToMeters that converts inches to meters after changing the input from string to double first

---
+++ src/introtojava/InchesToMeters.java
@@ -0,0 +1,18 @@
+package introtojava;
+import java.util.Scanner;
+
+public class InchesToMeters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input a value to be converted to meters:");
+		String input = scanner.nextLine();
+		double doubledInput = Double.parseDouble(input);
+		
+		System.out.println( doubledInput +" inches equals to " + (doubledInput * 0.0254) + " meters");
+				
+
+	}
+
+}
--- src/introtojava/calculator.java
+++ src/introtojava/calculator.java
@@ -19,7 +19,7 @@ public class calculator {
     double num1 = scanner.nextDouble();
     System.out.println("Enter the second number");
     double num2 = scanner.nextDouble();
-    System.out.println("The product of the numbers "+num1+" and "+num2+" is = "+(num1+num2));
+    System.out.println("The sum of the numbers "+num1+" and "+num2+" is = "+(num1+num2));
     System.out.println("To continue calculation Press 1 else Press any button to exit");
     int exit = scanner.nextInt();
     if (exit == 1) {
@@ -29,6 +29,88 @@ public class calculator {
      break;
     }
     }
+   else if(cal == 2) {
+	   System.out.println("Enter the first number");
+	   double num1 = scanner.nextDouble();
+	    System.out.println("Enter the second number");
+	    double num2 = scanner.nextDouble();
+	    System.out.println("The subtraction of the numbers "+num1+" and "+num2+" is = "+(num1 - num2));
+	    System.out.println("To continue calculation Press 1 else Press any button to exit");
+	    int exit = scanner.nextInt();
+	    if (exit == 1) {
+	     continue;
+	    }else {
+	     scanner.close();
+	     break;
+	    }
+	   
+   }
+   else if(cal == 3) {
+	   System.out.println("Enter the first number");
+	   double num1 = scanner.nextDouble();
+	    System.out.println("Enter the second number");
+	    double num2 = scanner.nextDouble();
+	    System.out.println("The products of the numbers "+num1+" and "+num2+" is = "+(num1 * num2));
+	    System.out.println("To continue calculation Press 1 else Press any button to exit");
+	    int exit = scanner.nextInt();
+	    if (exit == 1) {
+	     continue;
+	    }else {
+	     scanner.close();
+	     break;
+	    }
+	   
+   }
+   else if(cal == 4) {
+	   System.out.println("Enter the first number");
+	   double num1 = scanner.nextDouble();
+	    System.out.println("Enter the second number");
+	    double num2 = scanner.nextDouble();
+	    System.out.println("The division of the numbers "+num1+" and "+num2+" is = "+(num1 / num2));
+	    System.out.println("To continue calculation Press 1 else Press any button to exit");
+	    int exit = scanner.nextInt();
+	    if (exit == 1) {
+	     continue;
+	    }else {
+	     scanner.close();
+	     break;
+	    }
+	   
+   }
+   else if(cal == 5) {
+	   System.out.println("Enter the number to find Square");
+	   double num = scanner.nextDouble();
+	    System.out.println("The square of the number "+num+" is = "+(num * num));
+	    System.out.println("To continue calculation Press 1 else Press any button to exit");
+	    int exit = scanner.nextInt();
+	    if (exit == 1) {
+	     continue;
+	    }else {
+	     scanner.close();
+	     break;
+	    }
+   }
+   else if(cal == 6) {
+	   System.out.println("Enter the number to find Square root");
+	   double num = scanner.nextDouble();
+	   double numSquareRoot = Math.pow(num, 0.5);
+	    System.out.println("The square root of the number "+num+" is = "+(numSquareRoot));
+	    System.out.println("To continue calculation Press 1 else Press any button to exit");
+	    int exit = scanner.nextInt();
+	    if (exit == 1) {
+	     continue;
+	    }else {
+	     scanner.close();
+	     break;
+	    }
+	   
+	   
+   } else {
+	   System.out.println("Enter the number to find Reciprocal");
+	   double num = scanner.nextDouble();
+	  
+	   
+   }
   }
  }
 }
